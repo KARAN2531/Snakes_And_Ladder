7e0bd9a77ae0886bb60d1f3a328adabd77ab69d7
Karan-Gangwani31 <gangwanikaran7@gmail.com>
2025-04-17 00:50:18 +0530

    Merged UC4 into dev

--- snake-ladder-game.py
+++ snake-ladder-game.py
@@ -9,3 +9,12 @@ comp_dice_count = 0
 #UC2 - The Player rolls the die to get a number between 1 to 6.
 def roll_dice():
     return random.randint(1,6)
+
+#UC3 - The Player then checks for a option. They are No Play, Ladder or Snake.
+def check_movement():
+    lst = ["No Play", "Ladder", "Snake"]
+    return random.choice(lst)
+
+#UC4 - Repeat till the Player reacher the winning poistion 100. 
+def gameover():
+   return comp_position == 100 or user_position == 100
