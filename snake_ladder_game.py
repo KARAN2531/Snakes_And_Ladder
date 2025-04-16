fd413bbc4f0817dd19af2e9d19f530493102104c
Karan-Gangwani31 <gangwanikaran7@gmail.com>
2025-04-16 15:55:38 +0530

    UC-2

--- snake_ladder_game.py
+++ snake_ladder_game.py
@@ -4,13 +4,9 @@ import random
 user_position = 0
 comp_position = 0
 user_dice_count = 0
-<<<<<<< HEAD
-comp_dice_count = 0
-=======
 comp_dice_count = 0
 
 
 #UC2 - The Player rolls the die to get a number between 1 to 6.
 def roll_dice():
     return random.randint(1,6)
->>>>>>> UC2
