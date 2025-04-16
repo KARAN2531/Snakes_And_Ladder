27a11d3bc8956d8710d6a887e8ceb96c8dd3961c
Karan-Gangwani31 <gangwanikaran7@gmail.com>
2025-04-16 17:34:06 +0530

    UC2

--- snake-ladder-game.py
+++ snake-ladder-game.py
@@ -5,3 +5,7 @@ user_position = 0
 comp_position = 0
 user_dice_count = 0
 comp_dice_count = 0
+
+#UC2 - The Player rolls the die to get a number between 1 to 6.
+def roll_dice():
+    return random.randint(1,6)
